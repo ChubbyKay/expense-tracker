5330d8196833600556774a6f0b0acc0ebce3a948
Kay <kahachang@gmail.com>
2020-08-09 11:05:14 +0800

    mofidy: express routes & fix: edit function router

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 
 const Record = require('./models/record')
 const Category = require('./models/category')
-// const routes = require('./routes')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
@@ -29,67 +29,51 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-// app.use(routes)
+app.use(routes)
 
-// index page
-app.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .then(record => {
-      let totalAmount = 0
-      for (let i = 0; i < record.length; i++) {
-        totalAmount += record[i].amount
-      }
-      Category.find()
-        .lean()
-        .then(category => { res.render('index', { category, totalAmount, record }) })
-        .catch(error => console.log(error))
-    })
-    .catch(error => console.log(error))
-})
 
-// create page
-app.get('/records/new', (req, res) => {
-  res.render('new')
-})
+// // create page
+// app.get('/records/new', (req, res) => {
+//   res.render('new')
+// })
 
-// create function
-app.post('/records', (req, res) => {
-  const { name, category, date, amount } = req.body
-  return Record.create({ name, category, date, amount })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// // create function
+// app.post('/records', (req, res) => {
+//   const { name, category, date, amount } = req.body
+//   return Record.create({ name, category, date, amount })
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
-// edit
-app.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .lean()
-    .then((record) => res.render('edit', { record }))
-    .catch(error => console.log(error))
-})
+// // edit
+// app.get('/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   return Record.findById(id)
+//     .lean()
+//     .then((record) => res.render('edit', { record }))
+//     .catch(error => console.log(error))
+// })
 
-// edit update function
-app.put('/records/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .then(record => {
-      record = Object.assign(record, req.body)
-      return record.save()
-    })
-    .then(() => res.redirect(`/`))
-    .catch(error => console.log(error))
-})
+// // edit update function
+// app.put('/records/:id', (req, res) => {
+//   const id = req.params.id
+//   return Record.findById(id)
+//     .then(record => {
+//       record = Object.assign(record, req.body)
+//       return record.save()
+//     })
+//     .then(() => res.redirect(`/`))
+//     .catch(error => console.log(error))
+// })
 
-// delete
-app.delete('/records/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .then(record => record.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// // delete
+// app.delete('/records/:id', (req, res) => {
+//   const id = req.params.id
+//   return Record.findById(id)
+//     .then(record => record.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 app.listen(port, () => {
   console.log(`The app is running on localhost:${port}`)
