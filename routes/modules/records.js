c70f10379277935980f8102cb4b66d0e8564d58f
Kay <kahachang@gmail.com>
2020-08-12 12:59:18 +0800

    feat:增加新增頁面錯誤提示>項目填寫不齊全仍可新增

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -11,6 +11,25 @@ router.get('/new', (req, res) => {
 // create function
 router.post('/', (req, res) => {
   const { name, category, date, amount } = req.body
+
+  // 漏填資料提示
+  if (name.length === 0 || category.length === 0 || date.length === 0 || amount.length === 0) {
+    const alert = '請確實填寫每個項目'
+    return Record.find()
+      .lean()
+      .then(
+        res.render('new', { alert })
+      )
+      .catch(error => console.log(error))
+    // 金額錯誤提示
+  } else if (amount !== Number) {
+    const amountAlert = '金額請輸入數字,如 1200 '
+    return Record.find()
+      .lean()
+      .then(
+        res.render('new', { amountAlert })
+      )
+  }
   return Record.create({ name, category, date, amount })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
@@ -28,11 +47,15 @@ router.get('/:id/edit', (req, res) => {
 // edit update function
 router.put('/:id', (req, res) => {
   const id = req.params.id
+  const { name, category, date, amount } = req.body
+
   return Record.findById(id)
     .then(record => {
       record = Object.assign(record, req.body)
       return record.save()
     })
+
+
     .then(() => res.redirect(`/`))
     .catch(error => console.log(error))
 })
